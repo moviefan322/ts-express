6792128a0aa90007ad10851808f8440587389462
moviefan322 <philman202@yahoo.com>
2023-06-05 15:24:09 -0400

    connects routes and error handling

---
+++ dist/routes/todos.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+router.post('/', (req, res) => { });
+router.get('/', (req, res) => { });
+router.patch('/:id', (req, res) => { });
+router.delete('/:id', (req, res) => { });
+exports.default = router;
---
+++ src/routes/todos.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+
+const router = Router();
+
+router.post('/', (req, res) => {})
+
+router.get('/', (req, res) => {})
+
+router.patch('/:id', (req, res) => {})
+
+router.delete('/:id', (req, res) => {})
+
+export default router;
